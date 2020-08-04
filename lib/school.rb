3d7d243e3befddd96883434593e843f33ffc05ea
Anthony Lepore <violacellonyc@gmail.com>
2020-08-04 16:34:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,13 @@ class School
   end
   
   def add_student(student, grade)
-    roster[:grade]
-    roster[:student]
+    if roster.include?(grade)
+      roster[:grade].push(student)
+    else
+    roster[:grade] = []
+    roster[:grade].push(student)
+  end
+end
+
   
 end
